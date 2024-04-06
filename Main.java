2c4d709156f64e62c0c05980b4e40074b45c887e
Powerlifter2137 <keygle@protonmail.com>
2024-04-06 08:47:09 +0200

    changing code zad1

--- Main.java
+++ Main.java
@@ -1,3 +1,28 @@
 public class Main {
-  
+  public static void main(String[] args) {
+    Point newpoint = new Point(3, 5);
+    System.out.println(newpoint.getX());
+    System.out.println(newpoint.getY());
+  }
 }
+class Point {
+  private double x,y;
+
+  public Point(double x2, double y2){
+    x = x2;
+    y = y2;
+  }
+
+  public double getX(){
+    return x;
+  }
+  public double getY(){
+    return y;
+  }
+  public void setX(double x3){
+    x = x3;
+  }
+  public void setY(double y3){
+    y = y3;
+  }
+} 
