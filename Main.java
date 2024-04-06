2b3249cf8c4bda22356cadac2b798ddf5d006363
Powerlifter2137 <keygle@protonmail.com>
2024-04-06 10:17:43 +0200

    zad5 done

--- Main.java
+++ Main.java
@@ -1,23 +1,8 @@
 import geometry.*;
 public class Main {
   public static void main(String[] args) {
-      Circle[] circles = new Circle[3];
-        circles[0] = new Circle(new Point(2, 6), 2);
-        circles[1] = new Circle(new Point(1, 4), 1.0);
-        circles[2] = new Circle(new Point(-1, 4), 5.0);
-
-        ColoredCircle[] coloredCircles = new ColoredCircle[3];
-        coloredCircles[0] = new ColoredCircle(new Point(3, 7), 2.0, "red");
-        coloredCircles[1] = new ColoredCircle(new Point(0, 5), 1.5, "blue");
-        coloredCircles[2] = new ColoredCircle(new Point(-2, -1), 4, "green");
-
-        for (Circle circle : circles) {
-            System.out.println("pole: " + circle.calculateArea());
-        }
-
-        for (ColoredCircle coloredCircle : coloredCircles) {
-            System.out.println("pole: " + coloredCircle.calculateArea());
-            System.out.println("kolor: " + coloredCircle.getColor());
-        }
+    Rectangle rec1 = new Rectangle(2,3);
+    System.out.println(rec1.calculateArea());
+    System.out.println(rec1.calculatePerimeter());
   }
 }
