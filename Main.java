4606cfc362a71ee579f30fb8b313bbc7978bf67a
Powerlifter2137 <keygle@protonmail.com>
2024-04-06 10:22:15 +0200

    zad6 done

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 import geometry.*;
 public class Main {
   public static void main(String[] args) {
-    Rectangle rec1 = new Rectangle(2,3);
+    Square rec1 = new Square(4);
     System.out.println(rec1.calculateArea());
     System.out.println(rec1.calculatePerimeter());
   }
