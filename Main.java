7f85da903ef6c492a8dd35364a685a1eac7aabc8
Powerlifter2137 <keygle@protonmail.com>
2024-04-06 08:52:57 +0200

    zad1 point to new file

--- Main.java
+++ Main.java
@@ -5,24 +5,3 @@ public class Main {
     System.out.println(newpoint.getY());
   }
 }
-class Point {
-  private double x,y;
-
-  public Point(double x2, double y2){
-    x = x2;
-    y = y2;
-  }
-
-  public double getX(){
-    return x;
-  }
-  public double getY(){
-    return y;
-  }
-  public void setX(double x3){
-    x = x3;
-  }
-  public void setY(double y3){
-    y = y3;
-  }
-} 
---
+++ Point.java
@@ -0,0 +1,21 @@
+public class Point {
+  private double x,y;
+
+  public Point(double x2, double y2){
+    x = x2;
+    y = y2;
+  }
+
+  public double getX(){
+    return x;
+  }
+  public double getY(){
+    return y;
+  }
+  public void setX(double x3){
+    x = x3;
+  }
+  public void setY(double y3){
+    y = y3;
+  }
+} 
