ad2064bcb748b97a641eae4d03edb501008600f2
Powerlifter2137 <keygle@protonmail.com>
2024-04-06 09:49:05 +0200

    zad3 done

--- Main.java
+++ Main.java
@@ -2,10 +2,11 @@ import geometry.*;
 public class Main {
   public static void main(String[] args) {
       Point c = new Point(2, 3);
-      Circle circle1 = new Circle(c, 3.0);
+      ColoredCircle circle2 = new ColoredCircle(c, 3.0, "red");
 
-      System.out.println(circle1.getCenter().getX() + " " + circle1.getCenter().getY());
-      System.out.println(circle1.getRadius());
-      System.out.println(circle1.calculatePerimeter());
+      System.out.println(circle2.getCenter().getX() + " " + circle2.getCenter().getY());
+      System.out.println(circle2.getRadius());
+      System.out.println(circle2.calculatePerimeter());
+      System.out.println(circle2.getColor());
   }
 }
